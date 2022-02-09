4a947b21d5e50a2d84d71612d0ae624833eac4bd
Ola-22 <olanazly@gmail.com>
2022-02-09 23:16:46 +0200

    Add components to app file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Components/Header";
+import QuestionCard from "./Components/QuestionCard";
+import Choices from "./Components/Choices";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <QuestionCard />
+      <h5 className="title">المرشحين</h5>
+      <Choices />
     </div>
   );
 }
