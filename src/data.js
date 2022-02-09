a6a464c10dd1a46cc63a44e8816f47ee5592c8c5
Ola-22 <olanazly@gmail.com>
2022-02-09 23:20:25 +0200

    Add data file

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const data = {
+  Questions: [
+    {
+      _id: "1",
+      endVote: "ينتهي التصويت في ال 10 من نوفمبر 2022 الساعة 8 ص",
+      title: "برأيك من هو أفضل موظف في الشركة",
+    },
+  ],
+
+  Choices: [
+    {
+      _id: "1",
+      imgSrc: "/images/employee1.png",
+      name: "سارة شمالي",
+    },
+    {
+      _id: "2",
+      imgSrc: "/images/employee2.png",
+      name: "محمد ياسين",
+    },
+    {
+      _id: "3",
+      imgSrc: "/images/employee3.png",
+      name: "رامي جواد",
+    },
+    {
+      _id: "4",
+      imgSrc: "/images/employee4.png",
+      name: "محسن ابراهيم",
+    },
+  ],
+};
