ba8f2c3afacf5c3697bff72b5db6ce2f12cb6448
Ola-22 <olanazly@gmail.com>
2022-02-11 21:03:26 +0200

    Add component to the app file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import VoteMain from "./Pages/VoteMain";
-import ProgressBar from "./Components/ProgressBar";
 import ResultPage from "./Pages/ResultPage";
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
 
           <Route path="/" element={<Home />} />
         </Routes>
-        {/* <ProgressBar bgcolor="blue" progress="70" height={5} /> */}
       </div>
     </BrowserRouter>
   );
