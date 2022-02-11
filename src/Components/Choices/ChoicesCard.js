02ec58e2f7debade834f920b83f33e56ab752b60
Ola-22 <olanazly@gmail.com>
2022-02-11 21:12:38 +0200

    Add props to the choices card

--- src/Components/Choices/ChoicesCard.js
+++ src/Components/Choices/ChoicesCard.js
@@ -1,8 +1,32 @@
-export default function ChoicesCard({ src, name, onClick, className }) {
+export default function ChoicesCard({
+  src,
+  name,
+  onClick,
+  className,
+  progress,
+  voteNumber,
+  RateVote,
+}) {
   return (
     <div className={className} onClick={onClick}>
       <img src={src} alt="" />
-      <h4>{name}</h4>
+      <div
+      // style={{
+      //   display: "flex",
+      //   flexDirection: "column",
+      //   justifyContent: "center",
+      // }}
+      >
+        <h4>{name}</h4>
+        <p>{progress}</p>
+      </div>
+
+      <div>
+        <h6 style={{ fontSize: "16px", color: "#2f2f2f" }}>{RateVote}</h6>
+        <h5 style={{ fontSize: "12px", color: "#2f2f2f", opacity: "0.6" }}>
+          {voteNumber}
+        </h5>
+      </div>
     </div>
   );
 }
