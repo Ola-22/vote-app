bb85b2897ed226c1c56c797ba3ca2ddeb0ab146b
Ola-22 <olanazly@gmail.com>
2022-02-11 21:16:45 +0200

    filter data of the result by id

--- src/Pages/ResultPage.js
+++ src/Pages/ResultPage.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import ChoicesCard from "../Components/Choices/ChoicesCard";
 import Header from "../Components/Header";
 import QuestionCard from "../Components/QuestionCard";
 import { data } from "../data";
+import "../Components/Choices/style.css";
+import ProgressBar from "../Components/ProgressBar";
 
 export default function ResultPage() {
   const { id } = useParams();
@@ -14,7 +17,7 @@ export default function ResultPage() {
   return (
     <>
       <Header />
-      {data.Questions.map((question) => (
+      {result.map((question) => (
         <>
           <QuestionCard
             endVote={question.endVote}
@@ -22,9 +25,29 @@ export default function ResultPage() {
             imgSrc={question.imgSrc}
             componay={question.company}
           />
-          dd
         </>
       ))}
+      <h5 class="title">المرشحين</h5>
+      <div className="choices">
+        {data.Choices.map((choice) => (
+          <div key={choice._id}>
+            <ChoicesCard
+              className="choices-card card"
+              name={choice.name}
+              src={choice.imgSrc}
+              progress={
+                <ProgressBar
+                  bgcolor="blue"
+                  progress={choice.RateVote}
+                  height={5}
+                />
+              }
+              RateVote={choice.RateVote}
+              voteNumber={choice.voteNumber}
+            />
+          </div>
+        ))}
+      </div>
     </>
   );
 }
