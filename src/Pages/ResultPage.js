3f7979b111150d82d95799728acdd7c986db1da5
Ola-22 <olanazly@gmail.com>
2022-02-12 16:46:42 +0200

    Map data from localStorage

--- src/Pages/ResultPage.js
+++ src/Pages/ResultPage.js
@@ -6,46 +6,57 @@ import { data } from "../data";
 import "../Components/Choices/style.css";
 import ProgressBar from "../Components/ProgressBar";
 
-export default function ResultPage() {
+export default function ResultPage({ choice, setChoice, results, setResults }) {
   const { id } = useParams();
 
   const result = data.Questions.filter((question, index) => {
     return question._id === id;
   });
-
+  console.log(
+    Object.entries(JSON.parse(localStorage.getItem("vote-result"))).map(
+      ([key, val]) => {
+        return (
+          <p key={key}>
+            {key}: {val}
+          </p>
+        );
+      }
+    )
+  );
   return (
     <>
       <Header />
       {result.map((question) => (
-        <>
+        <div key={question._id}>
           <QuestionCard
             endVote={question.endVote}
             title={question.title}
             imgSrc={question.imgSrc}
             componay={question.company}
           />
-        </>
+        </div>
       ))}
-      <h5 class="title">المرشحين</h5>
+      <h5 className="title">المرشحين</h5>
       <div className="choices">
-        {data.Choices.map((choice) => (
-          <div key={choice._id}>
-            <ChoicesCard
-              className="choices-card card"
-              name={choice.name}
-              src={choice.imgSrc}
-              progress={
-                <ProgressBar
-                  bgcolor="blue"
-                  progress={choice.RateVote}
-                  height={5}
+        {Object.entries(JSON.parse(localStorage.getItem("vote-result"))).map(
+          ([key, val]) => {
+            return (
+              <>
+                <ChoicesCard
+                  className="choices-card card"
+                  name={key}
+                  src="/images/employee1.png"
+                  progress={
+                    <ProgressBar bgcolor="blue" progress={val * 3} height={5} />
+                  }
+                  per={`%`}
+                  RateVote={val * 3}
+                  voteNumber={`${val} صوت`}
                 />
-              }
-              RateVote={choice.RateVote}
-              voteNumber={choice.voteNumber}
-            />
-          </div>
-        ))}
+              </>
+            );
+          }
+        )}
       </div>
     </>
   );
