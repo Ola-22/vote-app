4d5ac0d2772c730a8436272537f6134c8f8de377
Ola-22 <olanazly@gmail.com>
2022-02-22 22:05:21 +0200

    import axios file, post data

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ import Home from "./Pages/Home";
 import VoteMain from "./Pages/VoteMain";
 import ResultPage from "./Pages/ResultPage";
 import QrCode from "./Pages/QrCode";
+// import getMAC, { isMAC } from "getmac";
+import { useState, useEffect } from "react";
+import axiosInstance from "./helpers/axios";
 
 function App() {
+  useEffect(() => {
+    axiosInstance
+      .post("/votes", { page_number: 1, page_size: 10 })
+      .then((res) => {
+        console.log("res", res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
   return (
     <BrowserRouter>
       <div className="App">
