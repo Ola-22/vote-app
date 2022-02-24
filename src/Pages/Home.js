c4e02e2a819daaf866d2a5c5aa8d18776fec6511
Ola-22 <olanazly@gmail.com>
2022-02-24 21:19:36 +0200

    use moment and get data from Api

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,27 +1,91 @@
 import { Link } from "react-router-dom";
-import Choices from "../Components/Choices";
 import Header from "../Components/Header";
 import QuestionCard from "../Components/QuestionCard";
-import { data } from "../data";
+import { useEffect, useState } from "react";
+import "./style.css";
+import moment from "moment";
 
-export default function Home() {
+export default function Home({ questions }) {
+  const [isActive, setActive] = useState(null);
+  const [choice, setChoice] = useState("");
+  const [results, setResults] = useState({});
+
+  const vote = () => {
+    if (!localStorage.getItem("vote-result")) {
+      localStorage.setItem("vote-result", JSON.stringify({}));
+    }
+    setResults({ ...results, [choice]: (results[choice] ?? 0) + 1 });
+  };
+
+  useEffect(() => {
+    localStorage.setItem("vote-result", JSON.stringify(results));
+  }, [results]);
+
+  function setActiveElement(id) {
+    setActive(id);
+  }
+
+  const [candidates, setCandidates] = useState();
+  const [currQues, setCurrQues] = useState(0);
+
+  // console.log(questions);
+
+  useEffect(() => {
+    setCandidates(questions && handleShuffle([questions[currQues]]));
+  }, [questions, currQues]);
+
+  const handleShuffle = (optionss) => {
+    return optionss.sort(() => Math.random() - 0.5);
+  };
+
   return (
     <div>
       <Header />
-      {data.Questions.map((question) => (
-        <div key={question._id}>
-          <Link to={`/vote-main/${question._id}`}>
+
+      {questions ? (
+        <div key={questions.id}>
+          <Link to={`/vote-main/${questions[currQues].id}`}>
             <QuestionCard
-              endVote={question.endVote}
-              title={question.title}
-              imgSrc={question.imgSrc}
-              componay={question.company}
+              question={questions[currQues].question}
+              end_at={moment(questions[currQues].end_at).format(
+                "MMMM Do YYYY h a"
+              )}
+              companyImg="/images/company.png"
+              company={questions[currQues].company}
             />
           </Link>
+
+          <h5 className="title">المرشحين</h5>
+
+          {questions[currQues].candidates.map((can) => (
+            <div className="choices">
+              <div
+                className={
+                  can.id === isActive ? "choices-card active" : "choices-card"
+                }
+                onClick={() => {
+                  setActiveElement(can.id);
+                  setChoice(can.name);
+                }}
+              >
+                <img src={can.photo} alt="" />
+                <h2>{can.name}</h2>
+              </div>
+            </div>
+          ))}
+          {isActive && (
+            <button
+              className="btn-vote"
+              onClick={vote}
+              style={{ backgroundColor: "#2e558d" }}
+            >
+              تأكيد
+            </button>
+          )}
         </div>
-      ))}
-      <h5 className="title">المرشحين</h5>
-      <Choices />
+      ) : (
+        <div>Loading... </div>
+      )}
     </div>
   );
 }
