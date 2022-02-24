17d91099f46680f64d21b6431cc795293427af05
Ola-22 <olanazly@gmail.com>
2022-02-24 21:13:59 +0200

    get data from Api and set state question

--- src/App.js
+++ src/App.js
@@ -4,30 +4,38 @@ import Home from "./Pages/Home";
 import VoteMain from "./Pages/VoteMain";
 import ResultPage from "./Pages/ResultPage";
 import QrCode from "./Pages/QrCode";
-// import getMAC, { isMAC } from "getmac";
 import { useState, useEffect } from "react";
 import axiosInstance from "./helpers/axios";
+import "./styles/main.css";
 
-function App() {
+function App({ candidates }) {
+  const [questions, setQuestions] = useState();
+
   useEffect(() => {
     axiosInstance
       .post("/votes", { page_number: 1, page_size: 10 })
       .then((res) => {
-        console.log("res", res);
+        setQuestions(res.data.items.data);
       })
       .catch((err) => {
         console.log(err);
       });
-  });
+  }, []);
 
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/vote-main" element={<VoteMain />} />
+          <Route
+            path="/vote-main"
+            element={<VoteMain questions={questions} />}
+          />
           <Route path="/vote-main/:id" element={<ResultPage />} />
           <Route path="/qr-code" element={<QrCode text="ola" />} />
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={<Home questions={questions} setQuestions={setQuestions} />}
+          />
         </Routes>
       </div>
     </BrowserRouter>
