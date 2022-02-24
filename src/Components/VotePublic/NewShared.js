daf23005c74c6470a5a7669533fb1ac275febb6f
Ola-22 <olanazly@gmail.com>
2022-02-24 21:20:20 +0200

    map data from api and slice of 5 questions

--- src/Components/VotePublic/NewShared.js
+++ src/Components/VotePublic/NewShared.js
@@ -1,19 +1,18 @@
-import { data } from "../../data";
 import QuestionCard from "../QuestionCard";
+import moment from "moment";
 
-export default function NewShared() {
+export default function NewShared({ questions }) {
+  const NewQue = questions?.slice(0, 5);
   return (
     <div>
-      {data.PublicShared.NewShared.map((question) => (
-        <>
-          <QuestionCard
-            endVote={question.endVote}
-            title={question.title}
-            img={question.img}
-            numberVote={question.numberVote}
-            numberTitle={question.numberTitle}
-          />
-        </>
+      {NewQue?.map((qu) => (
+        <QuestionCard
+          question={qu.question}
+          end_at={moment(qu.end_at).format("MMMM Do YYYY h a")}
+          img="/images/share.png"
+          numberTitle="عدد المصوتين"
+          // numberVote={qu.candidates.map.reduce((a, b) => a + b, 0)}
+        />
       ))}
     </div>
   );
