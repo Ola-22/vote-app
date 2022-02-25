05d5aebb166beac8ca656fbd5afe98554d715860
Ola-22 <olanazly@gmail.com>
2022-02-25 21:54:26 +0200

    Edit props number of the votes by reduce function

--- src/Components/VotePublic/MostShared.js
+++ src/Components/VotePublic/MostShared.js
@@ -8,15 +8,21 @@ export default function MostShared({ questions }) {
   return (
     <div>
       {MostQue?.map((qu) => (
-        <Link to={`/vote-main/${qu.id}`}>
+        <Link to={`/vote-main/${qu.id}`} key={qu.id}>
           <QuestionCard
             question={qu.question}
             end_at={moment(qu.end_at).format("Do MMMM YYYY h a")}
             img="/images/share.png"
             numberTitle="عدد المصوتين"
+            numberVote={
+              qu?.candidates
+                .map((can) => can.total_votes)
+                .reduce((a, b) => a + b)
+              // .sort((a, b) => b - a)
+            }
           />
         </Link>
-      ))}
+      )).sort((a, b) => b.ii - a.ii)}
     </div>
   );
 }
