42eaa2df147fb33be85e3b284787a88483e6ad4b
Ola-22 <olanazly@gmail.com>
2022-02-25 17:33:52 +0200

    Add state and localstorage, and generate unique id

--- src/App.js
+++ src/App.js
@@ -8,19 +8,58 @@ import { useState, useEffect } from "react";
 import axiosInstance from "./helpers/axios";
 import "./styles/main.css";
 
-function App({ candidates }) {
+function App() {
+  const [choice, setChoice] = useState("");
+  const [results, setResults] = useState({});
+
+  const vote = () => {
+    if (!localStorage.getItem("vote-result")) {
+      localStorage.setItem("vote-result", JSON.stringify({}));
+    }
+    setResults({ ...results, [choice]: (results[choice] ?? 0) + 1 });
+  };
+
+  useEffect(() => {
+    localStorage.setItem("vote-result", JSON.stringify(results));
+  }, [results]);
+
   const [questions, setQuestions] = useState();
+  const [macAddress, setMacAddress] = useState(null);
 
   useEffect(() => {
     axiosInstance
-      .post("/votes", { page_number: 1, page_size: 10 })
+      .post("/vote", { mac_address: macAddress, vote_id: choice })
       .then((res) => {
-        setQuestions(res.data.items.data);
+        // console.log("R", res.config);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }, [macAddress, choice]);
+
+  useEffect(() => {
+    axiosInstance
+      .post("/votes", { page_number: 1, page_size: 10, vote_id: choice })
+      .then((res) => {
+        setQuestions(res.data.items.data);
+        // console.log("RES", res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [choice]);
+
+  useEffect(() => {
+    const navigator_info = window.navigator;
+    const screen_info = window.screen;
+    const uid = navigator_info.mimeTypes.length;
+    const uidd = uid + navigator_info.userAgent.replace(/\D+/g, "");
+    const uuidd = uidd + navigator_info.plugins.length;
+    const uuiddd = uuidd + screen_info.height || "";
+    const uids = uuiddd + screen_info.width || "";
+    const newidd = uids + screen_info.pixelDepth || "";
+    setMacAddress(newidd);
+  }, [macAddress]);
 
   return (
     <BrowserRouter>
@@ -34,7 +73,9 @@ function App({ candidates }) {
           <Route path="/qr-code" element={<QrCode text="ola" />} />
           <Route
             path="/"
-            element={<Home questions={questions} setQuestions={setQuestions} />}
+            element={
+              <Home setChoice={setChoice} vote={vote} questions={questions} />
+            }
           />
         </Routes>
       </div>
